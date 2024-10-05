a5b95ff6e99e74795a2b7eaf7c3f15b409d2acac
Hoesu Chung <treecollector@yonsei.ac.kr>
2024-10-05 15:21:52 +0900

    UPDATE

---
+++ main.py
@@ -0,0 +1,20 @@
+def argparse():
+    pass
+
+def loss_function(recon_x, x, mu, logvar,lamb, mu_att, logvar_att):
+    pass
+
+def get_batches(iterable, n = flags.batch_size):
+    pass
+
+def train(epoch,lambda_kl):
+    pass
+
+def test(epoch,lambda_kl):
+    pass
+
+if __name__=='main':
+
+    #set optimizer
+  
+    pass
