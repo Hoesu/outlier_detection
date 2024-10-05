53ce1f654b3fa48444b17081bff7aaa2bc7bce33
Hoesu Chung <treecollector@yonsei.ac.kr>
2024-10-05 18:54:08 +0900

    UPDATE

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,3 +1,19 @@
+import os
+import json
+import pandas as pd
+
 class Dataset():
     def __init__(self, config):
-        pass
+        self.data_path = config['data_path']
+        self.dirc_name = self.data_path('/')[1]
+        self.file_name = self.data_path('/')[2]
+        self.standardize = config['standardize']
+
+        with open(config['interval_path'], 'r') as file:
+            self.intervals = json.load(file)['dirc_name']['file_name']
+
+    def load_csv(data_path: str) -> pd.DataFrame:
+        data = pd.read_csv(data_path)
+        return data
+    
+    
